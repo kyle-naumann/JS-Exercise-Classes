0ca0fcb7b32f4323f497c6425b6103aac9a0d999
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2020-08-14 12:41:53 -0700

    MVP finished. stupid misspelled words...

--- index.js
+++ index.js
@@ -131,8 +131,19 @@ speak(){
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+constructor(data){
+  super(data);
+  this.specialty = data.specialty;
+  this.favLanguage = data.favLanguage;
+  this.catchPhrase = data.catchPhrase;
+  }
+demo(subject){
+  return `Today we are learning about ${subject}`;
+  }
+grade(Student, subject){
+  return `${Student.name} recieves a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -150,8 +161,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+constructor(info){
+  super(info)
+  this.previousBackground = info.previousBackground
+  this.className = info.className
+  this.favSubjects = info.favSubjects
+}
+listSubjects(){
+return this.favSubjects;
+}
+PRAssignment(subject){
+return `${this.name} has submitted a PR for ${subject}`;
+}
+sprintChallenge(subject){
+return `${this.name} has begun sprint challenge on ${subject}`;
+}
 }
 
 /*
@@ -167,8 +192,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(arg){
+    super(arg)
+    this.gradClassName = arg.gradClassName
+    this.favInstructor = arg.favInstructor
+  }
+standUp(channel){
+  return `${this.name} announces to ${channel}, @channel standby times!`
+}
+debugsCode(student, subject){
+  return `${this.name} debugs ${student.name}'s code on ${subject}`
+}
 }
 
 /*
